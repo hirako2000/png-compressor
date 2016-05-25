e3efa151e7ce42b44d08f335bce1217f549ac35e
meed <meed2000@gmail.com>
2016-05-25 17:20:04 +0800

    Adds extra step to losslessly compress with PNGout

--- lossless.js
+++ lossless.js
@@ -1,6 +1,10 @@
 const imagemin = require('imagemin');
 const imageminOptipng = require('imagemin-optipng');
 
+var execFile = require('child_process').execFile;
+var pngout = require('pngout-bin');
+
+
 var args = process.argv.slice(2);
 if(args.length < 2) {
   console.log("Please pass two arguments");
@@ -13,10 +17,55 @@ if(args.length < 2) {
 var source = args[0] || './image/';
 var destination = args[1] || './optimized/';
 
+console.log("OptiPNGing all PNGs located in: " + source);
+console.log("Output for those will be located in: " + destination);
+
 imagemin([source + '/**/*.{jpg,png}'], destination, {
   plugins: [
     imageminOptipng({optimizationLevel : 3}) // default level is enough: 16 trial, could bump up to level 7: 240 trials
   ]
 }).then(files => {
-  console.log("Compressed losslessly, find files in " + destination);
+  if (files.length > 0) {
+    console.log("Now 2nd pass on each compressed image, with PNGOut this time");
+    pngoutit(0, files);
+  } else {
+    console.log("No file to pngout, DONE.");
+  }
 });
+
+var pngoutit = function(index, files) {
+  console.log("PNGouting: " + files[index].path);
+  execFile(pngout, [files[index].path, files[index].path + '.min.png', '-s0', '-k0', '-f0'], function (err) {
+      console.log('OK');
+      index++;
+      if (index >= files.length) {
+        console.log("DONE.");
+      } else {
+        pngoutit(index, files);
+      }
+  });
+}
+
+/* ---- PNG options: ----
+
+/f0	None
+/f1	Sub (delta x)
+/f2	Up (delta y)
+/f3	Average (delta x&y)
+/f4	Paeth
+/f5	Adaptive (mixed)
+/f6	Reuse (copy line by line from source PNG)
+For palette images /f0 is best, and /f5 is often the best for grayscale or true color images, but not always.
+The default value is /f0 for palette images (/c3), and /f5 for everything else.
+
+/s0	Xtreme! (Slowest)
+/s1	Intense (Slow)
+/s2	Longest Match (Fast)
+/s3	Huffman Only (Faster)
+/s4	Uncompressed (Fastest)
+
+/k0	Remove all unnecessary chunks. (default)
+/kp	Keep the palette order intact. Using this may hurt compression slightly,
+    but it may be necessary for certain applications (often on small electronic devices) that need to share a common palette.
+
+---- PNG options ---- */
--- lossly-then-losslessly.js
+++ lossly-then-losslessly.js
@@ -3,6 +3,8 @@ const imageminPngquant = require('imagemin-pngquant');
 const imageminOptipng = require('imagemin-optipng');
 
 const fs = require("fs");
+var execFile = require('child_process').execFile;
+var pngout = require('pngout-bin');
 
 var args = process.argv.slice(2);
 if(args.length < 3) {
@@ -30,14 +32,25 @@ imagemin([source + '/**/*.{jpg,png}'], destination, {
     ]
   }).then(files => {
     console.log("Optimized PNGs now Compressed, find files in " + finalDestination);
+    if (files.length > 0) {
+      console.log("Now 2nd pass on each compressed image, with PNGOut this time");
+      pngoutit(0, files);
+    } else {
+      console.log("No file to pngout, DONE.");
+    }
   });
 
 });
 
-
-
-
-
-
-
-
+var pngoutit = function(index, files) {
+  console.log("PNGouting: " + files[index].path);
+  execFile(pngout, [files[index].path, '-s0', '-k0', '-f0'], function (err) {
+      console.log('OK');
+      index++;
+      if (index >= files.length) {
+        console.log("DONE.");
+      } else {
+        pngoutit(index, files);
+      }
+  });
+}
